9c9164c3c6a9e931e0da32a579c7dd11c1a5072e
Myeonhwa <lyhwan5356@gmail.com>
2025-02-13 18:33:10 +0900

    qnaboard jusg save

---
+++ src/main/java/com/edu/springboot/qnaboard/IQACommentService.java
@@ -0,0 +1,15 @@
+package com.edu.springboot.qnaboard;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface IQACommentService {
+    List<QACommentDTO> getComments(@Param("qa_id") int qa_id);
+    int addComment(QACommentDTO comment);
+    int updateComment(QACommentDTO comment);
+    int deleteComment(@Param("comment_idx") int comment_idx);
+
+}
